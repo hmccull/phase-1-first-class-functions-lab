91c0afecf32a7c54993b6fdf4a3fb29c1daabb97
Hannah McCullough <hmac091@gmail.com>
2021-11-29 16:45:21 -0700

    commit tree b76d2ca7f6fa6dbfd2d302bcc5a0c9d91d5c8560

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-}
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
 
-const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-}
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(fareMultiplier) {
+const createFareMultiplier = function(fareMultiplier) {
   return value => fareMultiplier * value;
 }
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, driversToReturn) {
-    return driversToReturn(drivers);
-}
+const selectDifferentDrivers = (drivers, driversToReturn) => driversToReturn(drivers);
